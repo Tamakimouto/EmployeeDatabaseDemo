ac14745eba9e94368d9b655f6bb0aedfa7b9ce38
Anthony Zheng <Anthony@fopen-dream.space>
2017-02-13 17:24:09 -0500

    initial file structure set up & planning

---
+++ js/script.js
@@ -0,0 +1,21 @@
+$(function() {
+
+    var app = new Vue({
+        el: "#home",
+        data: {
+            queries: [
+                {name: "Department(s) with minimum number of employees", file: "php/file.php"},
+                {name: "Department(s) with maximum ratio of average female salaries to average men salaries", file: "php/file1.php"},
+                {name: "Manager(s) who held office for the longest duration", file: "php/file2.php"},
+                {name: "The number of employees born in each decade and their average salaries for each Dept", file: "php/file3.php"},
+                {name: "Female managers born before 1990, that make more than 80,000 a year", file: "php/file4.php"}
+            ],
+            result: ""
+        },
+        methods: {
+            runAjax: function(file) {
+                this.result = file;
+            }
+        }
+    });
+});
