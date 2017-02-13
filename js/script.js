a0d450199192d501001b606620a409489223071d
Anthony Zheng <Anthony@fopen-dream.space>
2017-02-13 18:30:06 -0500

    form section

--- js/script.js
+++ js/script.js
@@ -3,8 +3,9 @@ $(function() {
     var app = new Vue({
         el: "#home",
         data: {
+            customQuery: "Custom Table Join",
             queries: [
-                {name: "Department(s) with minimum number of employees", file: "php/file.php"},
+                {name: "Department(s) with minimum number of employees", file: "php/minemployee.php"},
                 {name: "Department(s) with maximum ratio of average female salaries to average men salaries", file: "php/file1.php"},
                 {name: "Manager(s) who held office for the longest duration", file: "php/file2.php"},
                 {name: "The number of employees born in each decade and their average salaries for each Dept", file: "php/file3.php"},
@@ -15,6 +16,9 @@ $(function() {
         methods: {
             runAjax: function(file) {
                 this.result = file;
+                $.ajax({
+                    url: file
+                })
             }
         }
     });
