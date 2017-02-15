c8926fa0355cdfeb58f58e9d62dd3bb25db204c3
EMILY JACKSON <emily@nike.cs.uga.edu>
2017-02-15 11:42:01 -0500

    Query2 still under construction

--- php/Query3.php
+++ php/Query3.php
@@ -7,29 +7,29 @@ Query3();
 /**
  * Query3
  *
- * Returns an array of the managers who hold office with the longest
+ * Returns an array of the manager who holds office with the longest
  * duration as JSON.
  *
  * Makes an SQL execution using the creds provided in dbconfig.php
- * fetches managers who have held office for the longest duration.
+ * fetches the manager who has held office for the longest duration.
  */
 function Query3() {
-    $db = connectDB();
-    $query = "SELECT MIN(empCount) FROM (SELECT dept_name, COUNT(dept_no) AS empCount FROM departments, employees, dept_emp WHERE employees.emp_no = dept_emp.emp_no AND dept_emp.dept_no = departments.dept_no)"; /* <--- TO DO -------- */
+	$db = connectDB();
+	$query = "SELECT first_name, last_name, TIMESTAMPDIFF(YEAR, from_date, to_date) AS years, TIMESTAMPDIFF(MONTH, from_date, to_date) % 12 AS months FROM employees, dept_manager WHERE employees.emp_no = dept_manager.emp_no AND TIMESTAMPDIFF(YEAR, from_date, to_date) > 8008;"; /* <--- TO DO -------- */
 
-    $prep = $db->prepare("$query");
-    $prep->execute();
+	$prep = $db->prepare("$query");
+	$prep->execute();
 
-    $result = array();
+	$result = array();
 
-    foreach($prep as $row) {
-        array_push($result, $row["DepartmentName"]); /* <----- TO DO (?) ---- */
-    }
+	foreach($prep as $row) {
+		array_push($result, $row["DepartmentName"]); /* <----- TO DO (?) ---- */
+	}
 
-    header("Content-Type: application/json");
-    echo json_encode($result);
+	header("Content-Type: application/json");
+	echo json_encode($result);
 
-    closeDB($db);
+	closeDB($db);
 
 }
 
